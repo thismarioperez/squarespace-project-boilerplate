9c37ad290d55aa7a1f7369df20f79ba3289ef8a6
Mario Perez <mario.perez415@gmail.com>
2017-10-24 17:10:27 -0400

    adds site styles to webpack build process

--- project_title/webpack.config.js
+++ project_title/webpack.config.js
@@ -1,36 +1,55 @@
 require('dotenv').config({ silent: true });
 
 const webpack = require('webpack');
+const ETP = require('extract-text-webpack-plugin');
 const path = require('path');
-
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const siteJs = 'site';
+const siteCss = 'site';
 
-const siteJs = ['./scripts/site.js'];
 
 const config = {
   devtool: IS_PRODUCTION ? false : 'inline-source-map',
-  entry: {
-    'scripts/site-bundle': siteJs
-  },
+  entry: './scripts/' + siteJs + '.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].js'
+    filename: 'scripts/' + siteJs + '-bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-            plugins: [
-              require('babel-plugin-add-module-exports'),
-              require('babel-plugin-transform-runtime')
-            ]
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env'],
+              plugins: [
+                require('babel-plugin-add-module-exports'),
+                require('babel-plugin-transform-runtime')
+              ]
+            }
           }
-        }
+        ]
+      },
+      {
+        test: /\.(css|less)$/,
+        loader: ETP.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader' },
+            { loader: 'postcss-loader',
+              options: {
+                plugins: () => [
+                  require('autoprefixer')({ browsers: ['last 2 versions'] }),
+                  require('cssnano')({})
+                ]
+              }
+            },
+            { loader: 'less-loader' }
+          ]
+        })
       }
     ]
   },
@@ -48,7 +67,9 @@ const config = {
       mangle: IS_PRODUCTION ? {
         except: ['_'] // don't mangle lodash
       } : false
-    })
+    }),
+
+    new ETP('./styles/' + siteCss + '.css')
   ]
 };
 
