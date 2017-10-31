4fb0ce06feea9b12038d6029de877fbfb7139927
Mario Perez <mario.perez415@gmail.com>
2017-10-31 15:41:17 -0400

    uses file-loader to place compiled less files in styles directory

--- project_title/webpack.config.js
+++ project_title/webpack.config.js
@@ -1,71 +1,15 @@
 require('dotenv').config({ silent: true });
-
+const path = require('path');
+const root = path.resolve(__dirname);
+const source = path.join(root, 'source');
+const nodeModules = 'node_modules';
 const webpack = require('webpack');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-const path = require('path');
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
-const config = {
+module.exports = {
   devtool: IS_PRODUCTION ? false : 'inline-source-map',
-  entry: {
-    app: path.resolve( __dirname, 'source/js/app.js' ),
-  },
-  output: {
-    path: path.resolve( __dirname, 'build', 'scripts' ),
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'eslint-loader',
-            options: {
-              configFile: './.eslintrc',
-              emitWarning: true,
-              emitError: true,
-              failOnError: IS_PRODUCTION ? true : false
-            }
-          }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env'],
-              plugins: [
-                require('babel-plugin-add-module-exports'),
-                require('babel-plugin-transform-runtime')
-              ]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(css|less)$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'postcss-loader',
-            options: {
-              plugins: () => [
-                require('autoprefixer')({ browsers: ['last 2 versions'] }),
-                require('cssnano')({})
-              ]
-            }
-          },
-          { loader: 'less-loader' }
-        ]
-      }
-    ]
-  },
+
   plugins: [
     new webpack.DefinePlugin({
       '__DEBUG__': JSON.stringify(!IS_PRODUCTION)
@@ -88,11 +32,52 @@ const config = {
       summary: false
     })
   ],
+
   stats: {
     env: true,
     modules: true,
     exclude: /node_modules/
+  },
+
+  resolve: {
+    modules: [root, source, nodeModules],
+    mainFields: ['webpack', 'browserify', 'web', 'main']
+  },
+
+  entry: {
+    app: path.resolve( __dirname, 'source/js/app.js' ),
+  },
+
+  output: {
+    path: path.resolve( __dirname, 'build', 'scripts' ),
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [
+      { test: /source\/js\/.*\.js$/, exclude: /node_modules/, use: ['eslint-loader'], enforce: 'pre' },
+      { test: /source\/js\/.*\.js$/, exclude: /node_modules/, use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+          plugins: [
+            require('babel-plugin-add-module-exports'),
+            require('babel-plugin-transform-runtime')] }
+      }] },
+      { test: /\.(css|less)$/,
+        use: [
+          'file-loader?name=../styles/[name].css',
+          { loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('autoprefixer')({ browsers: ['last 2 versions'] }),
+                require('cssnano')({})
+              ]
+            }
+          },
+          { loader: 'less-loader' }
+        ]
+      }
+    ]
   }
 };
-
-module.exports = config;
